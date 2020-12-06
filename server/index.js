55602d69b7127f2018393a61e27d852286b02f0f
TomboY90 <rjsahdia3@gmail.com>
2020-12-06 14:51:40 +0900

    fix heroku deploy

--- server/index.js
+++ server/index.js
@@ -16,9 +16,15 @@ app.use(cors());
 
 app.use('/posts', postRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello to my Memories API');
+})
+
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((error) => console.log('error', error.message))
 
   mongoose.set('useFindAndModify', false);
